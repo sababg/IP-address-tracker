274bb55ca53314be62801a4f2d28bea97d8ace8c
sababg <beigisaba@gmail.com>
2026-02-02 14:51:38 -0500

    add map and header

---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+// Fix for default marker icon - import as URLs
+const iconDefault = new L.Icon({
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
+L.Marker.prototype.options.icon = iconDefault;
+// Set default icon
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+// Initialize the map
+const map = L.map("map").setView([51.505, -0.09], 13);
+
+// Add tile layer from OpenStreetMap
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+// Add a marker
+const marker = L.marker([51.505, -0.09]).addTo(map);
+
+// Add a popup to the marker
+marker.bindPopup("<b>Hello!</b><br>This is your location.").openPopup();
